554c00794e09d310858c7b027d6c58019ddee095
Gustavo PEIXOTO DE OLIVEIRA <tavolesliv@gmail.com>
2016-02-08 00:53:04 -0300

    Minor modifications.

--- extractor_spe/py/conv_table_to_CMG.py
+++ extractor_spe/py/conv_table_to_CMG.py
@@ -1,15 +1,20 @@
+"""conv_table_to_CMG - write CMG-friendly file of coordinates."""
+
 import sys
 
 outfile = str(sys.argv[1]) + '_Parsed.txt'
 infile = str(sys.argv[1]) + '.txt'
 
-g = open(outfile,'w')
+g = open(outfile, 'w')
 
-with open(infile,'r') as f:
- next(f)
- for line in f:
-  x,y,z = line.split()
-  out = str(x)+':'+str(x)+' '+str(y)+':'+str(y)+' '+str(z)+':'+str(z)+' = 0\n'
-  g.write(out)
-  
+with open(infile, 'r') as f:
+    next(f)
+    for line in f:
+        x, y, z = line.split()
+        sx = str(x)+':'+str(x)
+        sy = str(y)+':'+str(y)
+        sz = str(z)+':'+str(z)
+        out = sx+' '+sy+' '+sz+' = 0\n'
+        g.write(out)
+
 g.close()
