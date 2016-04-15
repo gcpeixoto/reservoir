555c4e840464b4982e9ed79b48bcee5146ff2c5d
Gustavo PEIXOTO DE OLIVEIRA <tavolesliv@gmail.com>
2016-04-15 10:27:48 -0300

    Fixing error in saveExpurgatedIds.

--- lib/saveExpurgatedIds.m
+++ lib/saveExpurgatedIds.m
@@ -1,13 +1,18 @@
-function saveExpurgatedIds( idvec, i, j )
-% SAVEEXPURGATEDIDS write to file the reservoir's 
+function saveExpurgatedIds(idvec,i,j)
+% SAVEEXPURGATEDIDS write to file the well's 
 %      depth z-coordinates where a value of 0 was
 %      found for porosity. These values are removed in the analysis
 %      and this file is only intended to identify such points.
+%   
+%       input: 
+%              idvec: z coordinates array (nz x 1)
+%                i,j: well coordinates
 
-
+aux = [];
 for k=1:length(idvec)    
-    aux = [ i j idvec(k) ];
+    aux = [ aux; i j idvec(k) ];
     fname = fullfile( '../csv/',strcat( 'ExpurgatedPoints_I_',num2str(i),'J_',num2str(j),'.csv' ) );    
-    csvwrite(fname, aux);
+    dlmwrite(fname,aux);
+        
 end
 
