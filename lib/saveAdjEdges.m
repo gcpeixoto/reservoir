b21eef92031c824fe27014af73aec45f49b17a98
Gustavo PEIXOTO DE OLIVEIRA <tavolesliv@gmail.com>
2016-09-01 01:31:08 -0300

    Adding precision %d format to output edge file. This was paramount to solve a problem with SNAP. The centrality measures were not being correctly computed because the edge file was not with integer format. For very large clusters, more than 100.000, this was identified to not correctly assign the right amount of vertices.

--- lib/saveAdjEdges.m
+++ lib/saveAdjEdges.m
@@ -10,7 +10,7 @@ outfile = '../tmp/edges.txt';
 if issparse(Madj)
     [i,j] = find(Madj == 1);
     edges = [i,j];
-    dlmwrite(outfile,edges,'delimiter',' '); % writing file
+    dlmwrite(outfile,edges,'delimiter',' ','precision','%d'); % writing file
 else
     error('Matrix is not sparse');
 end
